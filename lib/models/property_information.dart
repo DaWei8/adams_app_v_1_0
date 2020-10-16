27a86106d2f14095a4495c2890159b2f90f52b96
David S <sawedavid7@gmail.com>
2020-10-16 17:42:45 +0100

    second attempted commit

--- lib/models/property_information.dart
+++ lib/models/property_information.dart
@@ -9,156 +9,151 @@ class PropertyInfo {
   final String buildingType;
   final List facilities;
 
-  PropertyInfo({this.location, this.picture, this.tenant, this.landLord, this.rent, this.numRooms, this.buildingType, this.facilities, this.propertyCode, });
+  PropertyInfo({
+    this.location,
+    this.picture,
+    this.tenant,
+    this.landLord,
+    this.rent,
+    this.numRooms,
+    this.buildingType,
+    this.facilities,
+    this.propertyCode,
+  });
 }
 
-List<PropertyInfo> propertyList  = [
+List<PropertyInfo> propertyList = [
   PropertyInfo(
-    propertyCode: 'ADM/589/AK/001',
-    picture: 'assets/images/blue_sky_bungalow.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
-    PropertyInfo(
-    propertyCode:  'ASM/236/BK/002',
-    picture: 'assets/images/house_garden.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
-   PropertyInfo(
-    propertyCode:  'AFM/683/CK/003',
-    picture: 'assets/images/house_on_hill.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
-   PropertyInfo(
-    propertyCode:  'ATM/453/TK/004',
-    picture: 'assets/images/blue_sky_bungalow.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
- PropertyInfo(
-    propertyCode:  'ATM/453/TK/005',
-    picture: 'assets/images/blue_sky_bungalow.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
- PropertyInfo(
-    propertyCode:  'ATM/453/TK/004',
-    picture: 'assets/images/blue_sky_bungalow.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
- PropertyInfo(
-    propertyCode:  'ATM/453/TK/004',
-    picture: 'assets/images/blue_sky_bungalow.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
- PropertyInfo(
-    propertyCode:  'ATM/453/TK/004',
-    picture: 'assets/images/blue_sky_bungalow.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
- PropertyInfo(
-    propertyCode:  'ATM/453/TK/004',
-    picture: 'assets/images/blue_sky_bungalow.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
- PropertyInfo(
-    propertyCode:  'ATM/453/TK/004',
-    picture: 'assets/images/blue_sky_bungalow.jpg',
-    location: 'Road no bending curve 23',
-    landLord: 'Master D',
-    tenant: 'Master Lee',
-    rent: 234566,
-    numRooms: 4,
-    buildingType: 'Bungalow',
-    facilities: [
-      'water',
-      'electricity',
-    ]
-  ),
-
+      propertyCode: 'ADM/589/AK/001',
+      picture: 'assets/images/blue_sky_bungalow.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 5,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'ASM/236/BK/002',
+      picture: 'assets/images/house_garden.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'AFM/683/CK/003',
+      picture: 'assets/images/house_on_hill.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'ATM/453/TK/004',
+      picture: 'assets/images/blue_sky_bungalow.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'ATM/453/TK/005',
+      picture: 'assets/images/blue_sky_bungalow.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'ATM/453/TK/004',
+      picture: 'assets/images/blue_sky_bungalow.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'ATM/453/TK/004',
+      picture: 'assets/images/blue_sky_bungalow.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'ATM/453/TK/004',
+      picture: 'assets/images/blue_sky_bungalow.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'ATM/453/TK/004',
+      picture: 'assets/images/blue_sky_bungalow.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
+  PropertyInfo(
+      propertyCode: 'ATM/453/TK/004',
+      picture: 'assets/images/blue_sky_bungalow.jpg',
+      location: 'Road no bending curve 23',
+      landLord: 'Master D',
+      tenant: 'Master Lee',
+      rent: 234566,
+      numRooms: 4,
+      buildingType: 'Bungalow',
+      facilities: [
+        'water',
+        'electricity',
+      ]),
 ];
-
-
-
-
 
 List pthAccounts = [
   'ADM/589/AK/001',
@@ -173,5 +168,4 @@ List pthAccounts = [
   'AZM/423/DK/010',
   'RZM/423/DK/012',
   'WZM/423/DK/013',
-
 ];
